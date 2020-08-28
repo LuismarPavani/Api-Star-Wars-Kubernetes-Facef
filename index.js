a78de7247754c31c883ae30820c6d70218d6adbb
LuismarPavani <luismar_pavani@hotmail.com>
2020-08-28 19:47:22 -0300

    Correção

--- index.js
+++ index.js
@@ -13,50 +13,42 @@ app.use((req, res, next) => {
   next();
 });
 
-const baseURL = 'https://swapi.dev/api/';
+const baseURL = 'https://swapi.co/api/';
 
 const getFilmId = (url) => {
-  const id = url.split('/')[5];
-  return Number(id);
-};
-
-const getFilmImageUrl = (id) => {
-  return `https://starwars-visualguide.com/assets/img/films/${id}.jpg`;
-};
-
-const getCharacterImageUrl = (url) => {
-  const getCharacterId = url.split('/')[5];
-  return `https://starwars-visualguide.com/assets/img/characters/${getCharacterId}.jpg`;
-};
-
-app.get('/films', async (req, res, next) => {
-  try {
-    const {
-      data: { results },
-    } = await axios.request({ baseURL, url: 'films' });
-    results.forEach((x) => (x.id = getFilmId(x.url)));
-    results.forEach((x) => (x.photo = getFilmImageUrl(x.id)));
-    return res.send(results).status(200);
-  } catch (error) {
-    console.error(error);
-    next(error);
+    const id = url.split('/')[5];
+    return Number(id);
   }
-});
+  const getCharacterImageUrl = (url) => {
+    const getCharacterId = url.split('/')[5];
+    return `https://starwars-visualguide.com/assets/img/characters/${getCharacterId}.jpg`;
+  }
 
-app.get('/films/:id', async (req, res, next) => {
-  try {
-    const filmId = req.params.id;
-    const { data } = await axios.request({ baseURL, url: `films/${filmId}` });
+  const getFilmImageUrl = (id) => {
+    return `https://starwars-visualguide.com/assets/img/films/${id}.jpg`;
+  }
 
-    const charactersRequests = await Promise.all(
-      data.characters.map((characterUrl) => {
+  app.get('/films', async (req, res, next) => {
+    try {
+      const { data: { results } } = await axios.request({ baseURL, url: 'films' });
+      results.forEach(x => x.id = getFilmId(x.url));
+      return res.send(results).status(200);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  });
+
+  app.get('/films/:id', async (req, res, next) => {
+    try {
+      const filmId = req.params.id;
+      const { data } = await axios.request({ baseURL, url: `films/${filmId}` });
+  
+      const charactersRequests = await Promise.all(data.characters.map(characterUrl => {
         return axios.get(characterUrl);
-      })
-    );
-
-    const characters = charactersRequests
-      .map((y) => y.data)
-      .map((x) => {
+      }));
+  
+      const characters = charactersRequests.map((y) => y.data).map((x) => {
         return {
           name: x.name,
           gender: x.gender,
@@ -64,26 +56,26 @@ app.get('/films/:id', async (req, res, next) => {
           eyeColor: x.eye_color,
           height: x.height,
           mass: x.mass,
-          photo: getCharacterImageUrl(x.url),
-        };
+          photo: getCharacterImageUrl(x.url)
+        }
       });
-
-    data.id = getFilmId(data.url);
-    data.photo = getFilmImageUrl(data.id);
-    data.characters = characters;
-
-    return res.send(data).status(200);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
-
-app.all('*', async (req, res, next) => {
-  res.send({
-    routes: ['films', 'films/id'],
+  
+      data.id = getFilmId(data.url);
+      data.photo = getFilmImageUrl(data.id);
+      data.characters = characters;
+  
+      return res.send(data).status(200);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
   });
-});
+
+  app.all('*', async (req, res, next) => {
+    res.send({
+      routes: ['films', 'films/id']
+    })
+  })
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
